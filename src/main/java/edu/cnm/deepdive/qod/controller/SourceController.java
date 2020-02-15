0b2632ffeab38cc665b7e9c3a6df4171d16903d7
mabbyreyes <mabbyreyes@gmail.com>
2020-02-15 14:52:14 -0700

    annotated with postmapping in source entity.

--- src/main/java/edu/cnm/deepdive/qod/controller/SourceController.java
+++ src/main/java/edu/cnm/deepdive/qod/controller/SourceController.java
@@ -3,12 +3,14 @@ package edu.cnm.deepdive.qod.controller;
 import edu.cnm.deepdive.qod.model.entity.Quote;
 import edu.cnm.deepdive.qod.model.entity.Source;
 import edu.cnm.deepdive.qod.service.SourceRepository;
+import java.net.URI;
 import java.util.Set;
 import java.util.UUID;
 import javax.print.attribute.standard.Media;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("sources")
@@ -30,11 +33,21 @@ public class SourceController {
     this.repository = repository;
   }
 
-@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
-    produces = MediaType.APPLICATION_JSON_VALUE)
-  public Source post(@RequestBody Source source) {
-    return repository.save(source);
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Source> post(@RequestBody Source source) {
+    repository.save(source);
+    URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+        .path("/{id}")
+        .build(source.getId());
+    return ResponseEntity.created(location).body(source);
   }
+
+//@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+//    produces = MediaType.APPLICATION_JSON_VALUE)
+//  public Source post(@RequestBody Source source) {
+//    return repository.save(source);
+//  }
 
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   public Iterable<Source> get() {
